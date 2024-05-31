6e4b1541bade2b22a0143886958fd671bd1b7f0a
Johnata Matheus <matheusabdala12@gmail.com>
2024-05-30 22:34:29 -0300

    feat: add case empty with styles

--- src/app/features/transaction/expense/components/expense-table/expense-table.component.ts
+++ src/app/features/transaction/expense/components/expense-table/expense-table.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Expense } from '../../models/Expense';
 import { ExpenseService } from '../../services/expense.service';
 import { CommonModule } from '@angular/common';
 import { TruncatePipe } from '../../../../../shared/pipes/truncate.pipe';
+import { ButtonComponent } from '../../../../../shared/components/button/button.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ModalTransactionComponent } from '../../../components/modal-transaction/modal-transaction.component';
+import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
 
 @Component({
   selector: 'app-expense-table',
   standalone: true,
-  imports: [CommonModule, TruncatePipe],
+  imports: [CommonModule, TruncatePipe, ButtonComponent],
   templateUrl: './expense-table.component.html',
   styleUrl: './expense-table.component.scss'
 })
-export class ExpenseTableComponent implements OnInit {
+export class ExpenseTableComponent implements OnChanges {
 
   expenses: Expense[] = [];
   
-  constructor(private expenseService: ExpenseService) {}
+  constructor(
+    private expenseService: ExpenseService,
+    private matDialog: MatDialog
+  ) {
+    this.getExpenses()
+  }
 
-  ngOnInit(): void {
-    this.getExpenses();
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
+
+  openModalExpense(){
+    const modalExpense = this.matDialog.open(ExpenseModalComponent);
+
+    modalExpense.componentInstance.success.subscribe(() => {
+      modalExpense.close();
+    })
   }
     
   getExpenses(){
+    this.expenseService.fetchUserExpenses();
     this.expenseService.getUserExpenses().subscribe({
       next: value => this.expenses = value,
       error: err => console.error("errooo!")
@@ -31,4 +49,10 @@ export class ExpenseTableComponent implements OnInit {
   getPaidStatus(isPaid: boolean): string {
     return isPaid ? "Pago" : "Não Pago";
   }
+
+
+  onExpenseCreated(): void {
+    this.getExpenses();
+  }
+
 }
