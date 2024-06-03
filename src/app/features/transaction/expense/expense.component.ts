8037c24e480837615ae7ae1ee64eebc82b65545e
Johnata Matheus <matheusabdala12@gmail.com>
2024-06-02 22:14:00 -0300

    feat: create expense component

---
+++ src/app/features/transaction/expense/expense.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from '../../../core/components/sidebar/sidebar.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { ExpenseTableComponent } from './components/expense-table/expense-table.component';
+import { Observable } from 'rxjs';
+import { Expense } from './models/Expense';
+import { ExpenseService } from './services/expense.service';
+
+
+
+@Component({
+  selector: 'app-expense',
+  standalone: true,
+  imports: [SidebarComponent, ButtonComponent, ExpenseTableComponent],
+  templateUrl: './expense.component.html',
+  styleUrl: './expense.component.scss'
+})
+export class ExpenseComponent {
+
+}
